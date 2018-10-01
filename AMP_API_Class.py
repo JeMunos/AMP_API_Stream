67cae37f61de66c19397071385353dda78526563
Jesse Munos <jemunos@cisco.com>
2018-10-01 12:02:56 -0700

    This is the initial commit of the AMP_API_Class, no code, just a problem definition.

---
+++ AMP_API_Class.py
@@ -0,0 +1,9 @@
+'''
+Author: Jesse Munos
+Date: October 2018
+
+This file will define a class and associated methods needed for interfacing
+with the Cisco AMP for Endpoints Streaming API.
+This API is used for ingesting AMP for Endpoints event data into various SIEM
+products such as ELK. 
+'''
